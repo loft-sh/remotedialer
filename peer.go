ad87435a0819aeb0afacdf4d0b83db370c65825b
Thomas Kosiewski <thomas.kosiewski@loft.sh>
2023-07-25 16:49:53 +0200

    Reverted back select default indentation
    Signed-off-by: Thomas Kosiewski <thomas.kosiewski@loft.sh>

--- peer.go
+++ peer.go
@@ -91,43 +91,44 @@ outer:
 		case <-ctx.Done():
 			break outer
 		default:
-			metrics.IncSMTotalAddPeerAttempt(p.id)
-			ws, _, err := dialer.Dial(p.url, headers)
-			if err != nil {
-				logger.Error(err, "Failed to connect to peer", "url", p.url, "id", s.PeerID)
-				time.Sleep(5 * time.Second)
-				continue
-			}
-			metrics.IncSMTotalPeerConnected(p.id)
-
-			session, err := NewClientSession(ctx, func(string, string) bool { return true }, ws)
-			if err != nil {
-				logger.Error(err, "Failed to connect to peer", "url", p.url)
-
-				time.Sleep(5 * time.Second)
-				continue
-			}
-
-			session.dialer = func(ctx context.Context, network, address string) (net.Conn, error) {
-				parts := strings.SplitN(network, "::", 2)
-				if len(parts) != 2 {
-					return nil, fmt.Errorf("invalid clientKey/proto: %s", network)
-				}
-				d := s.Dialer(parts[0])
-				return d(ctx, parts[1], address)
-			}
-
-			s.sessions.addListener(session)
-			_, err = session.Serve(ctx)
-			s.sessions.removeListener(session)
-			session.Close()
-
-			if err != nil {
-				logger.Error(err, "Failed to serve peer connection", "id", p.id)
-			}
-
-			ws.Close()
-			time.Sleep(5 * time.Second)
 		}
+
+		metrics.IncSMTotalAddPeerAttempt(p.id)
+		ws, _, err := dialer.Dial(p.url, headers)
+		if err != nil {
+			logger.Error(err, "Failed to connect to peer", "url", p.url, "id", s.PeerID)
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		metrics.IncSMTotalPeerConnected(p.id)
+
+		session, err := NewClientSession(ctx, func(string, string) bool { return true }, ws)
+		if err != nil {
+			logger.Error(err, "Failed to connect to peer", "url", p.url)
+
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		session.dialer = func(ctx context.Context, network, address string) (net.Conn, error) {
+			parts := strings.SplitN(network, "::", 2)
+			if len(parts) != 2 {
+				return nil, fmt.Errorf("invalid clientKey/proto: %s", network)
+			}
+			d := s.Dialer(parts[0])
+			return d(ctx, parts[1], address)
+		}
+
+		s.sessions.addListener(session)
+		_, err = session.Serve(ctx)
+		s.sessions.removeListener(session)
+		session.Close()
+
+		if err != nil {
+			logger.Error(err, "Failed to serve peer connection", "id", p.id)
+		}
+
+		ws.Close()
+		time.Sleep(5 * time.Second)
 	}
 }
